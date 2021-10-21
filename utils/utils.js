19d4023377cd9b6ab973d85642f88a918bcc98f0
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-10-21 16:24:01 +0000

    Add support for cookie auth in `getApi`

--- utils/utils.js
+++ utils/utils.js
@@ -4,9 +4,19 @@ import TatorApi from '../api/TatorApi';
 class Utils {
   static getApi(host='https://www.tatorapp.com', token=process.env.TATOR_TOKEN) {
     var defaultClient = ApiClient.instance;
-    var TokenAuth = defaultClient.authentications['TokenAuth'];
-    TokenAuth.apiKey = token;
-    TokenAuth.apiKeyPrefix = "Token";
+    if (token) {
+      var TokenAuth = defaultClient.authentications['TokenAuth'];
+      TokenAuth.apiKey = token;
+      TokenAuth.apiKeyPrefix = "Token";
+    } else {
+      let CookieAuth = defaultClient.authentications['CookieAuth'];
+      let value = "; " + document.cookie;
+      let parts = value.split("; csrftoken=");
+      if (parts.length == 2) {
+        value = parts.pop().split(";").shift();
+      }
+      CookieAuth.apiKey = value;
+    }
     defaultClient.basePath = host;
     return new TatorApi(defaultClient);
   }
