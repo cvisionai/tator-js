8701f4c74c2f67386fd0d0316415fc603371ff2a
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2022-10-28 08:43:02 +0000

    Unsuccessful attempt to unit test in node

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,11 +1,8 @@
 const base = require('@playwright/test');
+const http = require('http');
+const fs = require('fs');
 
 const tator = require('..');
-
-function returnIdPrintMessage(response) {
-  console.log(response.message);
-  return response.id;
-}
 
 // Host and token env vars
 exports.test = base.test.extend({
@@ -17,13 +14,30 @@ exports.test = base.test.extend({
     const api = tator.getApi(host, token);
     const date = new Date();
     const dateStr = date.toString();
-    const name = `test_project_${dateStr}`;
-    const id = await api.createOrganization({'name': name})
-                     .then(returnIdPrintMessage);
-    use(id);
+    const name = `test_org_${dateStr}`;
+    const organization = await api.createOrganization({'name': name});
+    await use(organization.id);
     if (!base.test.keep) {
-      await api.deleteOrganization(id);
+      await api.deleteOrganization(organization.id);
     }
   },
+  // Project ID
+  project: async ({host, token, organization}, use) => {
+    const api = tator.getApi(host, token);
+    const date = new Date();
+    const dateStr = date.toString();
+    const name = `test_project_${dateStr}`;
+    const project = await api.createProject({
+      name: name,
+      summary: `Test project created by tator-js unit tests on {dateStr}`,
+      organization: organization,
+    });
+    await use(project.id);
+    if (!base.test.keep) {
+      await api.deleteProject(project.id);
+    }
+  },
+  // Video URL
+  videoUrl: 'http://www.ballastmedia.com/wp-content/uploads/AudioVideoSyncTest_BallastMedia.mp4',
 });
 exports.expect = base.expect;
