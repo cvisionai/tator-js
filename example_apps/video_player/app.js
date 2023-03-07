874e49de15c8167e8596154961a5afde5c1b771d
Brian Tate <brian.tate@cvisionai.com>
2023-03-07 16:37:19 -0500

    Get example application working.

--- example_apps/video_player/app.js
+++ example_apps/video_player/app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'));
+function add_headers(res, path, stat)
+{
+  res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+}
+
+app.use(express.static('public', {setHeaders:add_headers}));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- example_apps/video_player/setup.sh
+++ example_apps/video_player/setup.sh
@@ -7,8 +7,10 @@ npm install express
 
 echo "Copying the development copy of tator.js, one could use npm here, potentially"
 cp -v ../../pkg/dist/tator.js public
-#cp -v ../../pkg/dist/0.tator.js public
-#cp -v ../../pkg/dist/src_annotator_vid_downloader_js.tator.js public
-#cp -v ../../pkg/dist/src_annotator_video-codec-worker_js-src_annotator_video-codec_js.tator.js public
+cp -v ../../pkg/dist/tator.min.js public
+cp -v ../../pkg/dist/0.tator.js public
+cp -v ../../pkg/dist/src_annotator_vid_downloader_js.tator.js public
+cp -v ../../pkg/dist/src_annotator_video-codec-worker_js-src_annotator_video-codec_js.tator.js public
+cp -v ../../src/annotator/vid_downloader.js public
 
 echo "Now run \"node app.js\" and the example is running at port 3000" 
