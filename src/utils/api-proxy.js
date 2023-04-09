9c305dc84d14d23ed30339e6c2a06fee7bbbc087
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2023-04-09 18:08:42 +0000

    Add call to refresh endpoint

--- src/utils/api-proxy.js
+++ src/utils/api-proxy.js
@@ -3,7 +3,7 @@ const handler = {
   get: function(target, prop, receiver) {
     const originalMethod = target[prop];
     if (typeof originalMethod === "function") {
-      return function (...args) {
+      return async function (...args) {
         // Check to see if we are using a JWT
         let accessToken = localStorage.getItem("access_token");
         let issueTime = localStorage.getItem("issue_time");
@@ -16,9 +16,24 @@ const handler = {
           const deltaMilliseconds = currentTime.getTime() - issueTime.getTime();
           const deltaSeconds = Math.floor(deltaMilliseconds / 1000);
           if (deltaSeconds > (expiresIn - 30)) {
-            // TODO: Do refresh here
+            const data = await fetch('/refresh')
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Refresh failed!");
+              }
+              return response.json();
+            })
+            .catch((error) => {
+              console.error("Error refreshing token!");
+              window.location.href = "/accounts/login";
+            });
+            localStorage.setItem("access_token", data.access_token);
+            localStorage.setItem("expires_in", data.expires_in);
+            localStorage.setItem("id_token", data.id_token);
+            localStorage.setItem("token_type", data.token_type);
+            localStorage.setItem("issue_time", issueTime.toISOString());
             let TokenAuth = target.apiClient.authentications['TokenAuth'];
-            TokenAuth.apiKey = accessToken;
+            TokenAuth.apiKey = data.access_token;
             TokenAuth.apiKeyPrefix = "Bearer";
           }
         }
